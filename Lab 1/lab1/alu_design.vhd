d57eb1b8e26dbc7d528f0a1cc3a910a5470cfe6f
rajjyarohan10 <rajjyarohan@gmail.com>
2023-09-18 23:21:33 -0400

    Final commit

--- Lab 1/lab1/alu_design.vhd
+++ Lab 1/lab1/alu_design.vhd
@@ -13,42 +13,53 @@ entity alu_design is
   port (
     A       : in std_logic_vector(3 downto 0);
     B       : in std_logic_vector(3 downto 0);
-    S1      : in std_logic;
+    S0      : in std_logic;
     S1      : in std_logic;
     G       : out std_logic_vector(3 downto 0);
+    Cin     : in std_logic;
     Cout    : out std_logic);
 end alu_design;
 
 architecture structural of alu_design is
 
-  component y_generator is 
+  component y_generator_comp 
     port(
       B     : in std_logic_vector(3 downto 0);
       S1    : in std_logic;
       S0    : in std_logic;
-      Y     : out std_logic_vector(3 downto 0);
-    );
+      Y     : out std_logic_vector(3 downto 0));
   end component;
 
-  component alu is
+  component alu_comp
     port (
       A     : in std_logic_vector(3 downto 0);
       Y     : in std_logic_vector(3 downto 0);
       Cin   : in std_logic;
       G     : out std_logic_vector(3 downto 0);
-      Cout  : out std_logic;
-    );
+      Cout  : out std_logic);
   end component;
 
   -- Use statements for each component specifying 
   -- which architecture to use
-  for all: y_generator use entity work.y_generator(structural);
-  for all: alu use entity work.alu(structural);
+  for all: y_generator_comp use entity work.y_generator(structural);
+  for all: alu_comp use entity work.alu(structural);
 
   signal Y: std_logic_vector(3 downto 0);
 
   begin
-    y_generator: y_generator port map (B=>B,S1=>S1,S=>S0,Y=>Y);
-    alu: alu port map (A=>A,Y=>Y,Cin=>Cin,G=>G,Cout=>Cout);
+    y_generator: y_generator_comp port map (
+      B => B,
+      S1 => S1,
+      S0 => S0,
+      Y => Y
+    );
+
+    alu: alu_comp port map (
+      A => A,
+      Y => Y,
+      Cin => Cin,
+      G => G,
+      Cout => Cout
+    );
 
 end structural;
--- Lab 1/lab1/alu_design_test_stdout.vhdl
+++ Lab 1/lab1/alu_design_test_stdout.vhdl
@@ -11,56 +11,66 @@ use IEEE.std_logic_arith.all;
 use STD.textio.all;
 
 entity alu_design_test_stdout is
-
+  
 end alu_design_test_stdout;
 
 architecture test of alu_design_test_stdout is
 
   signal A, B : std_logic_vector(3 downto 0);
-  signal S0, S1, Cin : std_logical;
+  signal S0, S1, Cin : std_logic;
   signal G : std_logic_vector(3 downto 0);
   signal Cout : std_logic;
-  
-  -- File I/O
-  file out_line : text open write_mode is "results.txt";
+
+  component alu_design
+    port(
+      A       : in std_logic_vector(3 downto 0);
+      B       : in std_logic_vector(3 downto 0);
+      S0      : in std_logic;
+      S1      : in std_logic;
+      Cin     : in std_logic;
+      G       : out std_logic_vector(3 downto 0);
+      Cout    : out std_logic
+    );
+  end component;
+
+begin
+
+  DUT: alu_design
+    port map(
+      A => A,
+      B => B,
+      S0 => S0,
+      S1 => S1,
+      Cin => Cin,
+      G => G,
+      Cout => Cout
+    );
+
+  -- Test Process
+  simulation_process: process
   variable line_buf : line;
-
+  file out_line : text open write_mode is "results.txt";
   begin
-    -- Instantiate alu_desgin entity design
-    DUT: entity work.alu_desgin
-      -- DUT: Device Under Test
-      port map(
-        A => A,
-        B => B,
-        S0 => S0,
-        S1 => S1,
-        Cin => Cin,
-        G => G,
-        Cout => Cout);
+    -- Initialize inputs
+    A <= "0000";
+    B <= "0000";
+    S0 <= '0';
+    S1 <= '0';
+    Cin <= '0';
+    wait for 100 ns;
 
-   -- Test Process
-    simulation_process: process
-    begin
-      -- Initialize inputs
-      A <= "0000";
-      B <= "0000";
-      S0 <= '0';
-      S1 <= '0';
-      Cin <= '0';
-      wait for 100 ns;
+    -- Test case 1
+    A <= "0101";
+    B <= "0011";
+    S0 <= '0';
+    S1 <= '0';
+    Cin <= '0';
+    wait for 100 ns;
 
-      -- Test case 1
-      A <= "0101";
-      B <= "0011";
-      S0 <= '0';
-      S1 <= '0';
-      Cin <= '0';
-      wait for 100 ns;
+    -- Stop the simulation
+    assert false
+    report "End of simulation" severity note;
+    wait;
 
-      -- Stop the simulation
-      assert false
-      report "End of simulation" severity note;
-      wait;
-
-    end process simulation_process;
+  end process simulation_process;
 end test;
